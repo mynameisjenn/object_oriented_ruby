151690c0a5f5662a6cee9156f1d25b370c89665d
mynameisjenn <jenniferlynnhatton@gmail.com>
2018-05-01 18:16:50 -0500

    changed file to idiomatic ruby

--- store_item.rb
+++ store_item.rb
@@ -2,16 +2,25 @@ class Store
   attr_reader :size, :color, :price
   attr_writer :price 
 
-  def initialize(size, color, price)
-    @size = size
-    @color = color
-    @price = price
+  def initialize(item_options)
+    @size = item_options[:size]
+    @color = item_options[:color]
+    @price = item_options[:price]
   end
 end
 
-item_1 = Store.new("small", "red", 3)
-item_2 = Store.new("med", "blue", 10)
-item_3 = Store.new("large", "purple", 20)
+item_1 = Store.new(
+                    size: "small", 
+                    color: "red", 
+                    price: 3)
+item_2 = Store.new(
+                    size: "med", 
+                    color: "blue", 
+                    price: 10)
+item_3 = Store.new(
+                    size: "large", 
+                    color: "purple", 
+                    price: 20)
 
 p item_1.price
 p item_2.color
